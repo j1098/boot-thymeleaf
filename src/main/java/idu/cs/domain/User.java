18217b3a4d89bc04baea221c2d7da6f5eb4b2651
lsj <sungjun203@gmail.com>
2019-05-29 21:12:49 +0900

    menu 수정 애노테이션

--- src/main/java/idu/cs/domain/User.java
+++ src/main/java/idu/cs/domain/User.java
@@ -1,5 +1,6 @@
 package idu.cs.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,21 +8,45 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user_table")
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id; 
-	// database에서 sequence number, primary key 역할
-	
+	// database에서 sequence number, auto increment => primary key 역할
+	@Column(nullable=false, length=20, unique=true)
+	private String userId;
+	private String userPw;
 	private String name;
 	private String company;
+	
+	
+	
+	public User(String userId, String userPw, String name, String company) {
+		super();
+		this.userId = userId;
+		this.userPw = userPw;
+		this.name = name;
+		this.company = company;
+	}
 	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userid) {
+		this.userId = userid;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userpw) {
+		this.userPw = userpw;
 	}
 	public String getName() {
 		return name;
